e5119c754bed3fad9e1842e3154685dace5fa5ea
Michael Green <michaeldangreen91@gmail.com>
2022-10-23 03:44:28 -0400

    updated html and started javascript

--- javascript.js
+++ javascript.js
@@ -1,1 +1,31 @@
+const month = ['January',
+                'February',
+                'March',
+                'April',
+                'May',
+                'June',
+                'July',
+                'August',
+                'September',
+                'October',
+                'November',
+                'December'
+            ];
 
+const d = new Date();
+let monthName = month[d.getMonth()];
+
+function displayMonth() {
+    document.getElementById("monthName").innerHTML = monthName;
+}
+displayMonth();
+
+function prevMonth() {
+    // takes the current month local time and goes back a month
+    document.getElementById("monthName").innerHTML = month[d.getMonth() - 1]
+}
+
+function nextMonth() {
+    // takes the current month local time and goes forward a month
+    document.getElementById("monthName").innerHTML = month[d.getMonth() + 1];
+}
